04143c65fc4726bba0aacf248904632a81db7cf2
karthikeyankuppu <43454404+karthikeyankuppu@users.noreply.github.com>
2020-11-18 13:44:32 +0530

    Update insert_function.sql

--- cartoDB_functions/insert_function.sql
+++ cartoDB_functions/insert_function.sql
@@ -3,9 +3,9 @@ DROP FUNCTION IF EXISTS insert_bikeways_data(text,text,text,text);
 
 CREATE OR REPLACE FUNCTION insert_bikeways_data (
     _geojson TEXT,
-    _description TEXT,
+    _notes TEXT,
     _name TEXT,
-    _zipcode TEXT)
+    _zip TEXT)
 --Has to return something in order to be used in a "SELECT" statement
 RETURNS integer
 AS $$
@@ -15,9 +15,9 @@ BEGIN
     --Convert the GeoJSON to a geometry type for insertion. 
     _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
 
-    EXECUTE ' INSERT INTO panaji_floods (the_geom, description, name, zipcode)
+    EXECUTE ' INSERT INTO panaji_floods (the_geom, notes, name, zipcode)
             VALUES ($1, $2, $3, $4)
-            ' USING _the_geom, _description, _name, _zipcode;
+            ' USING _the_geom, _notes, _name, _zip;
             
     RETURN 1;
 END;
